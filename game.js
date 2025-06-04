0c62c18a1b3199ecc2cfc242e5f5c76cec1d38e1
M3Coding <meady2009@gmail.com>
2025-06-04 15:48:37 -0400

    added random button color and switch statements to flash the random button

--- game.js
+++ game.js
@@ -1,2 +1,27 @@
 //alert("Welcome to Simon Game!")
 //$("h1").text("Welcome to Simon Game!");
+var gamePattern = [];
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    //console.log(randomChosenColor);
+    gamePattern.push(randomChosenColor);
+    switch (randomChosenColor) {
+        case "green":
+            $("#green").fadeOut(100).fadeIn(100);
+            break;
+        case "red":
+            $("#red").fadeOut(100).fadeIn(100);;
+            break;
+        case "yellow":
+            $("#yellow").fadeOut(100).fadeIn(100);;
+            break;
+        case "blue":
+            $("#blue").fadeOut(100).fadeIn(100);;
+            break;
+    }
+};
+
+nextSequence()
